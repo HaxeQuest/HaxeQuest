f76e6a06421fb4d0238df22d1dc1e5e7fd36db37
sayofthelor <35895538+sayofthelor@users.noreply.github.com>
2023-02-16 15:09:00 -0500

    work s

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,22 @@
 package;
 
+import scripting.HScriptBase;
+
 class PlayState extends BaseState {
+
+	var script:HScriptBase;
+	public static var instance:PlayState;
 	override public function create() {
+		instance = this;
 		super.create();
+		script = new HScriptBase("assets/scripts/test.hxs");
+		script.call("create");
+		script.call("postCreate");
 	}
 
 	override public function update(elapsed:Float) {
+		script.call("update", [elapsed]);
 		super.update(elapsed);
+		script.call("postUpdate", [elapsed]);
 	}
 }
---
+++ source/scripting/HScriptBase.hx
@@ -0,0 +1,58 @@
+package scripting;
+
+import hscript.Interp;
+import hscript.Parser;
+import flixel.util.FlxDestroyUtil.IFlxDestroyable;
+
+class HScriptBase implements IFlxDestroyable {
+    public function destroy() {
+        interp = null;
+        parser = null;
+    }
+
+    public final function set(k:String, v:Dynamic) {
+        if (interp != null) interp.variables.set(k, v);
+    }
+
+    public final function get(k:String) {
+        if (interp != null) return interp.variables.get(k);
+        return null;
+    }
+
+    public final function call(e:String, m:Array<Dynamic> = null) {
+        if (m == null) m = [];
+        interp.callMethod(e, m);
+    }
+
+    public final function doString(s:String) {
+        if (interp != null) try {
+            var e:hscript.Expr = parser.parseString(s);
+            interp.execute(e);
+        } catch(e:Dynamic) {openfl.Lib.application.window.alert(e, "HScript Error");}
+    }
+
+    public final function _trace(s:String) {
+        var posInfos:haxe.PosInfos = interp.posInfos();
+        Sys.println('${fileName}:${posInfos.lineNumber}: $s');
+    }
+
+    public var interp:Interp;
+    public var parser:Parser;
+    public var fileName:String;
+
+    public function new(fileName:String) {
+        interp = new Interp();
+        parser = new Parser();
+        this.fileName = fileName;
+        initVariables();
+        doString(sys.io.File.getContent(fileName));
+    }
+
+    public function initVariables() {
+        set("trace", _trace);
+        set("create", () -> {});
+        set("postCreate", () -> {});
+        set("update", (elapsed:Float) -> {});
+        set("postUpdate", (elapsed:Float) -> {});
+    }
+}
